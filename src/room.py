3c0a435afee6d3fd9ff54f29a5f09bf001c45105
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-07-08 18:21:09 -0700

    build room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    
+    def __init__(self, room_name, description):
+        self.room_name = room_name 
+        self.description = description
+        
+
+    def __repr__(self):
+        return '\nYour location is: %s, %s. \n' % (self.room_name, self.description)
+
