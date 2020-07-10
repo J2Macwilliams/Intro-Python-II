ad0010f157dfdec56af645162b1a7fc680f12e2f
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-07-10 13:36:42 -0700

    prompt new line

--- src/item.py
+++ src/item.py
@@ -5,5 +5,5 @@ class Item:
         self.description = description
         
     def __repr__(self):
-        return '\033[7m %s \033[0m -- %s' % (self.name, self.description)
+        return '\n\033[7m %s \033[0m -- %s\n' % (self.name, self.description)
 
