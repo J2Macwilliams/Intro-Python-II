2f9d00e557472da078f73ddc13a383b89f8b4da2
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-07-09 19:29:11 -0700

    add items attribute to player and my_items function

--- src/player.py
+++ src/player.py
@@ -6,6 +6,12 @@ class Player():
     def __init__(self, name, current_room ):
         self.name = name
         self.current_room = current_room
+        self.items = []
 
     def __str__(self):
         return ' %s' % (self.current_room)
+
+    def my_items(self):
+        print(f"I'm carrying: {self.items}") 
+
+    
